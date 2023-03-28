f44375a4adb19dba5bbdd3c97ba03db9ada298c9
Sir_46 <fadlan.8291@gmail.com>
2023-03-28 12:14:39 +0700

    Add mailTo

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+'use client';
 import { Button } from './ui/Button';
 
 type Props = {};
 
-const textSecondary = 'text-slate-800 dark:text-slate-200';
 const Index = (props: Props) => {
+  function mailTo() {
+    window.location.href = 'mailto:fadlan.jehteerokee@gmail.com';
+  }
   return (
     <section id='contact'>
       <div className='container px-6'>
@@ -16,13 +18,13 @@ const Index = (props: Props) => {
             Get In Touch
           </h2>
           <p
-            className={`w-full sm:w-2/3 md:w-2/3 lg:w-2/4 text-center ${textSecondary} mt-4`}
+            className={`w-full sm:w-2/3 md:w-2/3 lg:w-2/4 text-center text-secondary mt-4`}
           >
             Although I’m not currently looking for any new opportunities, my
             inbox is always open. Whether you have a question or just want to
             say hi, I’ll try my best to get back to you!
           </p>
-          <Button className='mt-4' variant='primary'>
+          <Button className='mt-4' variant='primary' onClick={() => mailTo()}>
             Say Hello
           </Button>
         </div>
