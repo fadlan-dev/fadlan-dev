859c49d18217ed1ef2df8112a2613e136487b23a
Sir_46 <fadlan.8291@gmail.com>
2023-11-06 12:35:01 +0700

    Add gtag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,21 @@ import WorkSection from '@/components/WorkSection';
 
 import projects from '@/constants/projects.json';
 import ContactSection from '@/components/ContactSection';
+import Script from 'next/script';
 
 export default function Home() {
   return (
     <main>
+      <Script src='https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID' />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-5N4MXQ8XVZ');
+        `}
+      </Script>
       <HeroSection />
       <AboutSection />
       <ExperienceSection />
