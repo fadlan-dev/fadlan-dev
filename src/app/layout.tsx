1d5ce8e722137be7a68a7b2f38187a6d6d3dda7f
Sir_46 <fadlan.8291@gmail.com>
2024-01-15 16:10:28 +0700

    Add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ServerThemeProvider } from '@wits/next-themes';
 import { IBM_Plex_Sans_Thai_Looped } from 'next/font/google';
 import ScollTopButton from '@/components/ScollTopButton';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const roboto = IBM_Plex_Sans_Thai_Looped({
   weight: ['400', '500', '700'],
@@ -25,6 +26,7 @@ export default function RootLayout({
     <ServerThemeProvider attribute='class'>
       <html lang='en' className={roboto.className}>
         <body>
+          <SpeedInsights />
           <ScollTopButton />
           <Navbar />
           {children}
