0d2975f025eb59eba97ec72d64e9635dd0301d6f
Sir_46 <fadlan.8291@gmail.com>
2024-03-16 15:41:09 +0700

    Remove console log

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,10 +12,6 @@ export const calcDate = (start: string, end: string | null) => {
   const startDate = dayjs(start).startOf('month');
   const endDate = end ? dayjs(end).endOf('month') : dayjs().endOf('month'); // If end is null, use current month
 
-  console.log('--------------------');
-  console.log('start', startDate);
-  console.log('end', endDate);
-
   // Calculate the difference in months
   const diffMonths = endDate.diff(startDate, 'month');
 
@@ -28,7 +24,6 @@ export const calcDate = (start: string, end: string | null) => {
 
   const years = Math.floor(diffMonthsAdjusted / 12);
   const remainingMonths = diffMonthsAdjusted % 12;
-  console.log('diffMonthsAdjusted', diffMonthsAdjusted);
 
   if (diffMonthsAdjusted > 12) {
     return `${years} yr ${remainingMonths ? `${remainingMonths} mos` : ''}`;
